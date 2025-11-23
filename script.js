5f80a372cdbb7813ec20db122859503e8bf6f27b
Harsh Vardhan <hv21082008@gmail.com>
2025-11-23 15:47:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ document.querySelectorAll('.card').forEach(card=>{
   });
 
   card.addEventListener('mousemove', e=>{
-    let rect = card.getBoundingClientRect();
-    let x = e.clientX - rect.left;
-    let y = e.clientY - rect.top;
-    let rotY = (x / rect.width - 0.5) * 20;
-    let rotX = -(y / rect.height - 0.5) * 20;
-    card.style.transform = `rotateX(${rotX}deg) rotateY(${rotY}deg)`;
+    let rect=card.getBoundingClientRect();
+    let x=e.clientX-rect.left;
+    let y=e.clientY-rect.top;
+
+    let rotY=(x/rect.width - 0.5)*30;
+    let rotX=-(y/rect.height - 0.5)*30;
+
+    card.style.transform=`rotateX(${rotX}deg) rotateY(${rotY}deg) scale(1.08)`;
   });
 
   card.addEventListener('mouseleave',()=>{
-    card.style.transform = "rotateX(0deg) rotateY(0deg)";
+    card.style.transform="rotateX(0deg) rotateY(0deg)";
   });
 });
